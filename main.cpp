d75278d34ade361966eddf7528785aeada58356f
NadaAlinour <lime.x.na@gmail.com>
2025-12-06 22:32:05 +0200

    Add method to compare tiles with goal state

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Board
 {
     int n; // size of n-puzzle
-    vector<vector<int>> board;
+    vector<vector<int>> tiles;
+    Board* parent; // pointer to the current state's parent in order to get the path later(?)
 
 public:
     // get size of board and the initial state (as an array) to populate the matrix
@@ -17,7 +21,7 @@ public:
     Board(int n, vector<int> state)
     {
         this->n = n;
-        this->board.resize(n, vector<int>(n));
+        this->tiles.resize(n, vector<int>(n));
 
         cout << "Initializing board.." << "\n";
 
@@ -26,21 +30,45 @@ public:
         {
             for (int j = 0; j < n; j++)
             {
-                this->board[i][j] = state[k++];
+                this->tiles[i][j] = state[k++];
 
-                cout << this->board[i][j] << " ";
+                cout << this->tiles[i][j] << " ";
             }
 
             cout << "\n";
         }
             
     }
-        
+
+    // method to print the state of the board (node)
+    void printState() {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << this->tiles[i][j] << " ";
+            }
+
+            cout << "\n";
+        }
+    }
+
+    // method to test if current tiles match goal tiles
+    // returns 1 if equal, 0 otherwise
+    int isGoal() {
+        vector<vector<int>> goalState = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+        return this->tiles == goalState;
+    }
+
+    
 };
+
+
 
 int main()
 {
-    cout << "Enter n-puzzle size: " << "\n";
+    /*cout << "Enter n-puzzle size: " << "\n";
     int n;
     cin >> n;
 
@@ -49,12 +77,14 @@ int main()
 
     for(int i = 0; i < n*n; i++) {
         cin >> initialState[i];
-    }
+    }*/
 
-    //int n = 3;
-    //vector<int> initialState = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    int n = 3;
+    vector<int> initialState = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
     Board newBoard(n, initialState);
+    int isEqual = newBoard.isGoal();
+
 
     return 0;
 }
